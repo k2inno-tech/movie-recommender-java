61cf7a111a975942b6981f5d06608f8597b414fe
kkloke <kokkeong.loke@modefair.com>
2018-07-27 07:42:59 +0800

    Init project.

---
+++ src/main/java/app/MovieRecommender.java
@@ -0,0 +1,16 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.Movie;
+import app.User;
+
+public class MovieRecommender {	
+	public List<Movie> recommendMovies(User user, int returnCount) {
+		// Implementation here
+
+
+		return new ArrayList<Movie>();
+	}
+}
