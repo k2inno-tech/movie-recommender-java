e1173dd86030f0073068a501e28bef0cff91b437
kkloke <kokkeong.loke@modefair.com>
2018-07-27 10:25:24 +0800

    Tidy up.

--- src/main/java/app/controller/MovieRecommender.java
+++ src/main/java/app/controller/MovieRecommender.java
@@ -1,15 +1,14 @@
-package app;
+package app.controller;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import app.Movie;
-import app.User;
+import app.model.Movie;
+import app.model.User;
 
 public class MovieRecommender {	
 	public List<Movie> recommendMovies(User user, int returnCount) {
 		// Implementation here
-
 
 		return new ArrayList<Movie>();
 	}
